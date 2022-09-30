1c935fb359dc9e15cfff3c00926971eb5c90cb9a
Luis Arrecis <luisarrecis2@gmail.com>
2022-09-29 23:46:43 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoAnalisis2G8/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAnalisis2G8.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Diary()
+        {
+            return View();
+        }
+
+        public ActionResult Mayor()
+        {
+            return View();
+        }
+
+        public ActionResult FinancialStatus()
+        {
+            return View();
+        }
+        public ActionResult AnnualAccounts()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoAnalisis2G8/Controllers/RecordsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAnalisis2G8.Controllers
+{
+    public class RecordsController : Controller
+    {
+        // GET: Records
+
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+        public ActionResult Nomenclature()
+        {
+            return View();
+        }
+        public ActionResult CostCenter()
+        {
+            return View();
+        }
+    }
+}
